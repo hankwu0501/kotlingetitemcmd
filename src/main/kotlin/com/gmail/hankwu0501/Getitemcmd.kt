eff3dab81094dd1ed827597c2f3d956f6d16069f
hankwu0501 <hankwu0501@gmail.com>
2021-07-25 17:27:53 +0800

    修復插件建構時的問題

--- src/main/kotlin/com/gmail/hankwu0501/Getitemcmd.kt
+++ src/main/kotlin/com/gmail/hankwu0501/Getitemcmd.kt
@@ -3,7 +3,7 @@ package com.gmail.hankwu0501
 import com.gmail.hankwu0501.command.Command
 import org.bukkit.plugin.java.JavaPlugin
 
-class Main: JavaPlugin() {
+class Getitemcmd: JavaPlugin() {
     override fun onEnable() {
         print("插件啟動成功")
         getCommand("getitemcmd")?.setExecutor(Command)
